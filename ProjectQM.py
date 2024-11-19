a25cc538b7a472a5503f46cf6eba97e64e78b585
Ruslan Masinjila <ruslanmasinjila@gmail.com>
2024-11-19 17:44:04 -0500

    ProjectQM

--- ProjectQM.py
+++ ProjectQM.py
@@ -53,7 +53,7 @@ strTimeframe   = ['M1','M2','M3','M4','M5','M6','M10','M12','M15','M20','M30','H
 
 
 num_candles         = 100
-offset              = 1
+offset              = 0
 sleep_time          = 5
 
 ##########################################################################################
@@ -202,7 +202,7 @@ def get_signals():
             if(first_sequence_is_green and second_sequence_is_red and third_sequence_is_green and fourth_sequence_is_red and fifth_sequence_is_green):
                 if((first_sequence_lowest_low > third_sequence_highest_high  and
                     first_sequence_lowest_low > fourth_sequence_highest_high and 
-                    first_sequence_lowest_low > fifth_sequence_highest_high)):
+                    first_sequence_highest_high > fifth_sequence_highest_high)):
                     if((fourth_sequence_lowest_low < first_sequence_lowest_low  and
                         fourth_sequence_lowest_low < second_sequence_lowest_low and 
                         fourth_sequence_lowest_low < third_sequence_lowest_low  and
@@ -214,7 +214,7 @@ def get_signals():
             if(first_sequence_is_red and second_sequence_is_green and third_sequence_is_red and fourth_sequence_is_green and fifth_sequence_is_red):
                 if((first_sequence_highest_high < third_sequence_lowest_low  and
                     first_sequence_highest_high < fourth_sequence_lowest_low and 
-                    first_sequence_highest_high < fifth_sequence_lowest_low)):
+                    first_sequence_lowest_low < fifth_sequence_lowest_low)):
                     if((fourth_sequence_highest_high > first_sequence_highest_high  and
                         fourth_sequence_highest_high > second_sequence_highest_high and 
                         fourth_sequence_highest_high > third_sequence_highest_high  and
