82cee53c3dc9e71250cfcaf6cab43076c117d626
Ruslan Masinjila <ruslanmasinjila@gmail.com>
2024-12-01 20:49:59 -0500

    ProjectSE

--- ProjectSE.py
+++ ProjectSE.py
@@ -202,9 +202,10 @@ def get_signals():
                     first_sequence_highest_high > third_sequence_highest_high   and
                     first_sequence_highest_high > fourth_sequence_highest_high)): 
                     if(fourth_sequence_lowest_low < second_sequence_lowest_low and fourth_sequence_highest_high > second_sequence_highest_high):
-                        if(length_fourth_sequence <= length_second_sequence):
-                            signal = 'BUY '
-                            beep = 1
+                        if(fourth_sequence_highest_high > third_sequence_highest_high):
+                            if(length_fourth_sequence <= length_second_sequence):
+                                signal = 'BUY '
+                                beep = 1
                 
 
             if((first_sequence_is_green     and 
@@ -215,9 +216,10 @@ def get_signals():
                     first_sequence_lowest_low < third_sequence_lowest_low   and
                     first_sequence_lowest_low < fourth_sequence_lowest_low)):
                     if(fourth_sequence_highest_high > second_sequence_highest_high and fourth_sequence_lowest_low < second_sequence_lowest_low): 
-                        if(length_fourth_sequence <= length_second_sequence):
-                            signal = 'SELL'
-                            beep = 1
+                        if( fourth_sequence_lowest_low < third_sequence_lowest_low):
+                            if(length_fourth_sequence <= length_second_sequence):
+                                signal = 'SELL'
+                                beep = 1
 
 
 
