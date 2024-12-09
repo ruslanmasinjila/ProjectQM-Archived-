e5f4b21b775ccfb6f65f9f9c193ffe0c175b4267
Ruslan Masinjila <ruslanmasinjila@gmail.com>
2024-12-08 20:10:37 -0500

    ProjectQM

--- ProjectQM.py
+++ ProjectQM.py
@@ -195,41 +195,31 @@ def get_signals():
                 second_sequence_is_green    and 
                 third_sequence_is_red       and 
                 fourth_sequence_is_green)):
-                if(second_sequence_lowest_low < first_sequence_lowest_low):
-                    if(fourth_sequence_lowest_open < second_sequence_lowest_low and fourth_sequence_highest_close > second_sequence_highest_high):
-                        if(length_fourth_sequence <= length_second_sequence):
-                            signal = 'BUY '
-                            beep = 1
-                            if((first_sequence_highest_high > second_sequence_highest_high and
-                                first_sequence_highest_high > third_sequence_highest_high  and
-                                first_sequence_highest_high > fourth_sequence_highest_high )):
+                if((first_sequence_highest_high > second_sequence_highest_high and
+                    first_sequence_highest_high > third_sequence_highest_high  and
+                    first_sequence_highest_high > fourth_sequence_highest_high )):
+                    if(second_sequence_lowest_low < first_sequence_lowest_low):
+                        if(fourth_sequence_lowest_open < second_sequence_lowest_low and fourth_sequence_highest_close > second_sequence_highest_high):
+                            if(length_fourth_sequence <= length_second_sequence):
                                 difference = abs((first_sequence_highest_high - fourth_sequence_highest_high)/(symbol_info.point)) - spread
                                 if(difference >= 10):
-                                    signal = signal + "*"
+                                    signal = 'BUY '
+                                    beep = 1
             
             if((first_sequence_is_green     and 
                 second_sequence_is_red      and 
                 third_sequence_is_green     and 
                 fourth_sequence_is_red)):
-                if(second_sequence_highest_high > first_sequence_highest_high):
-                    if(fourth_sequence_highest_open > second_sequence_highest_high and fourth_sequence_lowest_close < second_sequence_lowest_low):
-                        if(length_fourth_sequence <= length_second_sequence):
-                            signal = 'SELL'
-                            beep = 1
-                            if((first_sequence_lowest_low < second_sequence_lowest_low and
-                                first_sequence_lowest_low < third_sequence_lowest_low  and
-                                first_sequence_lowest_low < fourth_sequence_lowest_low )):
+                if((first_sequence_lowest_low < second_sequence_lowest_low and
+                    first_sequence_lowest_low < third_sequence_lowest_low  and
+                    first_sequence_lowest_low < fourth_sequence_lowest_low )):
+                    if(second_sequence_highest_high > first_sequence_highest_high):
+                        if(fourth_sequence_highest_open > second_sequence_highest_high and fourth_sequence_lowest_close < second_sequence_lowest_low):
+                            if(length_fourth_sequence <= length_second_sequence):
                                 difference = abs((first_sequence_lowest_low - fourth_sequence_lowest_low)/(symbol_info.point)) - spread
                                 if(difference >= 10):
-                                    signal = signal + "*"
-
-
-                    
-
-
-
-
-            
+                                    signal = 'SELL'
+                                    beep = 1
 
             ##########################################################################################
             
